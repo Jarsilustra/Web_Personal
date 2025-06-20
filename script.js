7cb6ae012fae7785eda8a29647356fe25121f898
Jarsilustra <andrews8810@gmail.com>
2025-06-20 17:18:25 -0500

    responsive index

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.querySelectorAll(".flip-card").forEach((card) => {
     card.classList.toggle("flipped");
   });
 });
+
+/// ---- Menu ----
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".menu-toggle");
+  const menuList = document.querySelector(".menu-list");
+
+  toggle.addEventListener("click", function () {
+    menuList.classList.toggle("active");
+  });
+});
