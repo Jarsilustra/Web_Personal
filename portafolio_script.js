330159bcfe2323319a79d40b6c61159645c14a41
Jarsilustra <andrews8810@gmail.com>
2025-06-27 15:48:27 -0500

    Correcciones Listas

--- portafolio_script.js
+++ portafolio_script.js
@@ -1,5 +1,3 @@
-const { lazy } = require("react");
-
 const swiper = new Swiper(".mySwiper", {
   loop: true,
   slidesPerView: 3,
@@ -46,23 +44,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /// ---- Modal ----
 
-  document.querySelectorAll(".swiper-slide img").forEach((img) => {
-    img.addEventListener("click", () => {
-      const modal = document.getElementById("modal");
-      const modalImg = document.getElementById("imgAmpliada");
-      modal.style.display = "block";
-      modalImg.src = img.src;
-      modalImg.alt = img.alt;
-    });
-  });
-
-  document.querySelector(".close").addEventListener("click", () => {
-    document.getElementById("modal").style.display = "none";
-  });
-
-  window.addEventListener("click", (event) => {
+document.querySelectorAll(".swiper-slide img").forEach((img) => {
+  img.addEventListener("click", () => {
     const modal = document.getElementById("modal");
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
+    const modalImg = document.getElementById("imgAmpliada");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
   });
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  document.getElementById("modal").style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
